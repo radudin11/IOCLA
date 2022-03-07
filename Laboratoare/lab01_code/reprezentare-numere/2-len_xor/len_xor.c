71387a16f789a002f69739b9f2e97e4b5eb2ad9a
Dinicescu Radu <rodede.radu@gmail.com>
2022-03-07 15:33:40 +0200

    finisheed equality_check

--- Laboratoare/lab01_code/reprezentare-numere/2-len_xor/len_xor.c
+++ Laboratoare/lab01_code/reprezentare-numere/2-len_xor/len_xor.c
@@ -21,6 +21,13 @@ void equality_check(const char *str)
 {
 	/* TODO */
 	(void) str;
+	int len = my_strlen(str);
+	for (int i = 0; i < len; i++) {
+		int i_toCompare = (i + (1 << i)) % len;
+		if (!(*(str + i) ^ (*(str + i_toCompare)))) {
+			printf("Address of %c: %p, is equal to: %c, %p\n", *(str+i), (str+i), *(str+i_toCompare), (str + i_toCompare));
+		}
+	}
 }
 
 int main(void)
@@ -28,8 +35,9 @@ int main(void)
 	/* TODO: Test functions */
 
 	// test my_strlen
-	char* sir = "123";
-    printf("%d", my_strlen(sir));
+	char* sir = "ababababacccbacbacbacbacbabc";
+    printf("length = %d", my_strlen(sir));
+	equality_check(sir);
     return 0;
 }
 
