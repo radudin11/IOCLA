4b5eb43ce32c94b85ecffd8119d47d11394640af
Dinicescu Radu <rodede.radu@gmail.com>
2022-03-07 22:36:51 +0200

    finished rotations.c

--- Laboratoare/lab01_code/reprezentare-numere/4-rotations/rotations.c
+++ Laboratoare/lab01_code/reprezentare-numere/4-rotations/rotations.c
@@ -3,21 +3,41 @@
 void rotate_left(int *number, int bits)
 {
 	/* TODO */
-	(void) number;
-	(void) bits;
+	int mask = 1 << 30;
+	while(bits) {
+		int add = 0;
+		if(mask & *number)
+			add = 1;
+		*number <<= 1;
+		*number += add;
+		bits--;
+	}
+	 
 }
 
 void rotate_right(int *number, int bits)
 {
 	/* TODO */
-	(void) number;
-	(void) bits;
+	int mask = 1 << 30;
+	while(bits) {
+		int add = 0;
+		if(mask & *number)
+			add = 1 << 30;
+		*number >>= 1;
+		*number += add;
+		bits--;
+	}
+
 }
 
 int main()
 {
 	/* TODO: Test functions */
-
+	int a = 321421432;
+	int b = 321421432;
+	rotate_left(&a, 1);
+	rotate_right(&b, 1);
+	printf("%d %d\n", a, b);
 	return 0;
 }
 
