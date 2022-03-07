4d22eba79d2cb5544ab246c6ce52b930d978a7a6
Dinicescu Radu <rodede.radu@gmail.com>
2022-03-07 15:49:54 +0200

    finished mirror.c

--- Laboratoare/lab01_code/reprezentare-numere/3-mirror/mirror.c
+++ Laboratoare/lab01_code/reprezentare-numere/3-mirror/mirror.c
@@ -5,12 +5,27 @@
 void mirror(char *s)
 {
 	/* TODO */
-	(void) s;
+
+	char *start = s;
+	char *end = NULL;
+	for (end = s; *end != '\0'; end++);
+	end--;
+	while(end > start) {
+		printf("end = %p, start = %p\n	", end, start);
+		char aux = *end;
+		*end = *start;
+		*start = aux;
+		start++;
+		end--;
+	}
 }
 
 int main()
 {
 	/* TODO: Test function */
+	char sir[20] = "AnaAreMere";
+	mirror(sir);
+	printf("%s\n", sir);
 
 	return 0;
 }
